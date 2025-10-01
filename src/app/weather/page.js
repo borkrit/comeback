5a50fc5c6c7395b35e7dd61490789595616c310a
Vladyslav Pohorilyi <pogoriliy15@gmail.com>
2025-10-02 01:39:25 +0200

    update file

--- src/app/weather/page.js
+++ src/app/weather/page.js
@@ -1,14 +1,8 @@
-import Cart from "@/components/Cart/Cart";
 
 const Page = ()=>{
     return (
         <div className="page">
-            details
-            {
-                Array(5).fill(0).map((item,i)=>{
-                    return <Cart key={i} />
-                })
-            }
+
         </div>
     )
 }
