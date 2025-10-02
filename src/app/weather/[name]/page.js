5f4e0470e23e15d4856a9e6bc6dc5fdcb2e62a1d
Vladyslav Pohorilyi <pogoriliy15@gmail.com>
2025-10-02 02:06:02 +0200

    update file

--- src/app/weather/[name]/page.js
+++ src/app/weather/[name]/page.js
@@ -1,10 +1,8 @@
-import Link from "next/link";
 import Details from "@/components/Details/Details";
 
 
 export default async function Page({params}) {
 const {name} = params;
-
 
     return (
         <div>
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -16,7 +16,7 @@ const Search = () => {
     useEffect(() => {
         async function fetchData() {
             setLoading(true)
-            const data = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${API}`);
+            const data = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${name}&limit=5&appid=${API}`);
             const res = await data.json();
             setLists(res)
             setLoading(false)
